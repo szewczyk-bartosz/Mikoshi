ce118c1e3703f802e1b9506fa13a45787dbcff8d
szewczyk-bartosz <cheryllamb123098@protonmail.com>
2025-12-27 11:30:05 +0000

    cleanup

--- sddm-theme/theme.conf.nix
+++
@@ -1,9 +0,0 @@
-{ theme, wallpaper }:
-''
-[General]
-background=${wallpaper}
-primaryColor=${theme.colours.primary}
-secondaryColor=${theme.colours.secondary}
-errorColor=${theme.colours.error}
-fontFamily=${theme.fonts.main}
-''
