2234f8357c9fb53c36b10daeb91db1c61eb25abb
Bartosz Szewczyk <cheryllamb123098@protonmail.com>
2025-12-27 05:09:08 +0000

    a little neovim tweak

--- modules/home/nixvim/ui.nix
+++ modules/home/nixvim/ui.nix
@@ -114,6 +114,37 @@ in
               end
             '';
           };
+          highlights = {
+            buffer_selected = {
+              fg = "${selectedTheme.colours.primary}";
+              bold = true;
+              italic = false;
+            };
+            numbers_selected = {
+              fg = "${selectedTheme.colours.primary}";
+              bold = true;
+            };
+            diagnostic_selected = {
+              fg = "${selectedTheme.colours.primary}";
+              bold = true;
+            };
+            hint_selected = {
+              fg = "${selectedTheme.colours.primary}";
+              bold = true;
+            };
+            info_selected = {
+              fg = "${selectedTheme.colours.primary}";
+              bold = true;
+            };
+            warning_selected = {
+              fg = "${selectedTheme.colours.primary}";
+              bold = true;
+            };
+            error_selected = {
+              fg = "${selectedTheme.colours.primary}";
+              bold = true;
+            };
+          };
         };
       };
 
